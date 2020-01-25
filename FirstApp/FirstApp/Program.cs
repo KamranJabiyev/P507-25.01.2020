e6b23611a812de32807a25568c0d12739e35b854
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2020-01-25 20:08:53 +0400

    Add files via upload

---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Variables
+            //int x = 100;
+            //x = 1050000;
+            //string word = "Hello";
+            //bool isMarried = true;
+            //var y = 1000;
+            //var word2 = "test";
+            //float z = 10.2F;
+            //decimal d = 100.5M;
+            //double dd = 50.5;
+            #endregion
+
+
+            //Console.WriteLine("Daxil etdiyiniz eded:"+result);
+            //Console.WriteLine($"Daxil etdiyiniz eded: {result}");
+
+            //Console.WriteLine("Eded daxil edin:");
+            //string result = Console.ReadLine();
+            #region Try,catch
+            //try
+            //{
+            //    int res = int.Parse(result);
+
+            //    while (res <= 18)
+            //    {
+
+            //        Console.WriteLine("Zehmet olmasa 18-den boyuk eded daxil edin");
+            //        result = Console.ReadLine();
+            //        res=int.Parse(result);
+            //    }
+            //    Console.WriteLine($"Daxil etdiyiniz eded: {res}");
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("Daxil etdiyiniz eded deyil");
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Finally");
+            //}
+            #endregion
+
+            #region Array,Loops,If
+            //int[] numbers = { 15, 35, 24, 12 };
+            //int[] numbers = new int[] { 15, 'a',17};
+            //int x = 5;
+            //string[] words = { "Mubariz","Hemid"};
+
+            //foreach (int num in numbers)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            //string name = "Mubariz";
+
+            //for (int i = 0; i < name.Length; i++)
+            //{
+            //    Console.WriteLine(name[i]);
+            //}
+
+            //foreach (char letter in name)
+            //{
+            //    if (letter == 'z') Console.WriteLine(letter);
+            //}
+            #endregion
+
+            #region Value,references types
+
+            //int a = 5;
+            //int b = a;
+            //b = 10;
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //int[] n1 = { 10, 20, 30 };
+            //int[] n2 = n1;
+            //n2[0] = 100;
+            //Console.WriteLine(n1[0]);
+            //Console.WriteLine(n2[0]);
+
+            #endregion
+
+        }
+
+    }
+}
+
---
+++ P507Library/P507Library/Class1.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace P507Library
+{
+    public class Animal
+    {
+        private protected string Type { get; set; }
+        protected string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class Test:Animal
+    {
+        public Test()
+        {
+            Type = "";
+        }
+    }
+}
